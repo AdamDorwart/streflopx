53f25e53f343c8839f13ebd0a62bf9b8825b91ab
Adam Dorwart <adamjdorwart@gmail.com>
2024-09-24 11:29:25 -0700

    Namespace

--- arithmeticTest.cpp
+++ arithmeticTest.cpp
@@ -223,8 +223,8 @@ template<class FloatType> void doTest(string s, string name) {
     uint16_t lastMXCSR = getMXCSR();
     logFPCR(lastFPCR, lastFPCR, "Initial");
     logMXCSR(lastMXCSR, lastMXCSR, "Initial");
-    std::cout << FE_ALL_EXCEPT << std::endl;
-    std::cout << FE_INVALID << std::endl;
+    std::cout << streflop::FE_ALL_EXCEPT << std::endl;
+    std::cout << streflop::FE_INVALID << std::endl;
     feclearexcept(streflop::FE_ALL_EXCEPT);
     // Trap NaNs
     feraiseexcept(streflop::FE_INVALID);
